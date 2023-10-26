d44e04863c8551b14b9ae63cf5c149629c8c5289
Погадайко Антон <pogadajko52@gmail.com>
2023-10-26 21:41:59 +0300

    create init Registration page layout

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,7 +1,70 @@
 import { FC } from "react";
 
 const Registration: FC = () => {
-  return <h1>Registration</h1>;
+  return (
+    <div className="registration">
+      <section className="registration__container">
+        <div className="registration__body">
+          <h1 className="registration__title">Registration</h1>
+          <form className="registration__form">
+            <fieldset className="registration__section">
+              <input
+                type="text"
+                className="registration__input"
+                placeholder="Username"
+                tabIndex={1}
+              />
+            </fieldset>
+            <fieldset className="registration__section">
+              <input
+                type="text"
+                className="registration__input"
+                placeholder="Email"
+                tabIndex={2}
+              />
+            </fieldset>
+            <fieldset className="registration__section">
+              <input
+                type="text"
+                className="registration__input"
+                placeholder="Password"
+                tabIndex={3}
+              />
+            </fieldset>
+            <fieldset className="registration__section">
+              <input
+                type="text"
+                className="registration__input"
+                placeholder="Password"
+                tabIndex={3}
+              />
+            </fieldset>
+            <fieldset className="registration__section">
+              <input
+                type="text"
+                className="registration__input"
+                placeholder="Passport number"
+                tabIndex={4}
+              />
+            </fieldset>
+            <fieldset className="registration__section">
+              <input
+                type="text"
+                className="registration__input"
+                placeholder="Tel"
+                tabIndex={5}
+              />
+            </fieldset>
+            <fieldset className="registration__section">
+              <button type="submit" className="registration__button">
+                Register
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Registration;
