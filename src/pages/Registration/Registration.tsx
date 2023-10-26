298b58607bb61554e6247649213ada986910670a
Погадайко Антон <pogadajko52@gmail.com>
2023-10-26 22:27:12 +0300

    fix  outline styles to focus effect

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,67 +1,58 @@
 import { FC } from "react";
+import cl from "./Registration.module.scss";
 
 const Registration: FC = () => {
   return (
-    <div className="registration">
-      <section className="registration__container">
-        <div className="registration__body">
-          <h1 className="registration__title">Registration</h1>
-          <form className="registration__form">
-            <fieldset className="registration__section">
-              <input
-                type="text"
-                className="registration__input"
-                placeholder="Username"
-                tabIndex={1}
-              />
-            </fieldset>
-            <fieldset className="registration__section">
-              <input
-                type="text"
-                className="registration__input"
-                placeholder="Email"
-                tabIndex={2}
-              />
-            </fieldset>
-            <fieldset className="registration__section">
-              <input
-                type="text"
-                className="registration__input"
-                placeholder="Password"
-                tabIndex={3}
-              />
-            </fieldset>
-            <fieldset className="registration__section">
-              <input
-                type="text"
-                className="registration__input"
-                placeholder="Password"
-                tabIndex={3}
-              />
-            </fieldset>
-            <fieldset className="registration__section">
-              <input
-                type="text"
-                className="registration__input"
-                placeholder="Passport number"
-                tabIndex={4}
-              />
-            </fieldset>
-            <fieldset className="registration__section">
-              <input
-                type="text"
-                className="registration__input"
-                placeholder="Tel"
-                tabIndex={5}
-              />
-            </fieldset>
-            <fieldset className="registration__section">
-              <button type="submit" className="registration__button">
-                Register
-              </button>
-            </fieldset>
-          </form>
-        </div>
+    <div className={cl["registration"]}>
+      <section className={cl["registration__body"]}>
+        <h1 className={cl["registration__title"]}>Registration</h1>
+        <form className="registration__form">
+          <fieldset className={cl["registration__section"]}>
+            <input
+              type="text"
+              className={cl["registration__input"]}
+              placeholder="Username"
+              tabIndex={1}
+            />
+          </fieldset>
+          <fieldset className={cl["registration__section"]}>
+            <input
+              type="text"
+              className={cl["registration__input"]}
+              placeholder="Email"
+              tabIndex={2}
+            />
+          </fieldset>
+          <fieldset className={cl["registration__section"]}>
+            <input
+              type="text"
+              className={cl["registration__input"]}
+              placeholder="Password"
+              tabIndex={3}
+            />
+          </fieldset>
+          <fieldset className={cl["registration__section"]}>
+            <input
+              type="text"
+              className={cl["registration__input"]}
+              placeholder="Passport number"
+              tabIndex={4}
+            />
+          </fieldset>
+          <fieldset className={cl["registration__section"]}>
+            <input
+              type="text"
+              className={cl["registration__input"]}
+              placeholder="Tel"
+              tabIndex={5}
+            />
+          </fieldset>
+          <fieldset className={cl["registration__section"]}>
+            <button type="submit" className={cl["registration__button"]}>
+              Register
+            </button>
+          </fieldset>
+        </form>
       </section>
     </div>
   );
