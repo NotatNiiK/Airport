5ecf914c3e3e093bd2c77329aa6cb78b2b3779d8
Погадайко Антон <pogadajko52@gmail.com>
2023-10-26 23:39:48 +0300

    add init registration form validation

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,16 +1,50 @@
 import { FC } from "react";
 import cl from "./Registration.module.scss";
+import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
+import RegValidation from "../../validation/RegValidation";
+import { InputMask } from "@react-input/mask";
+interface RegForm {
+  fullname: string;
+  email: string;
+  password: string;
+  passportNumber: string;
+  contactInfo: string;
+}
 
 const Registration: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+    reset,
+  } = useForm<RegForm>();
+
+  const performRegistration: SubmitHandler<RegForm> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  const errorRegistration: SubmitErrorHandler<RegForm> = (data) => {};
+
   return (
     <div className={cl["registration"]}>
       <section className={cl["registration__body"]}>
         <h1 className={cl["registration__title"]}>Registration</h1>
-        <form className="registration__form">
+        <form
+          className="registration__form"
+          onSubmit={handleSubmit(performRegistration, errorRegistration)}
+        >
           <fieldset className={cl["registration__section"]}>
             <input
               type="text"
               className={cl["registration__input"]}
+              {...register("fullname", {
+                required: true,
+                validate: RegValidation.fullname,
+              })}
+              style={{ boxShadow: errors.fullname ? "0px 0px 5px red" : "" }}
+              onBlur={() => clearErrors("fullname")}
               placeholder="Username"
               tabIndex={1}
             />
@@ -19,6 +53,12 @@ const Registration: FC = () => {
             <input
               type="text"
               className={cl["registration__input"]}
+              {...register("email", {
+                required: true,
+                validate: RegValidation.email,
+              })}
+              style={{ boxShadow: errors.email ? "0px 0px 5px red" : "" }}
+              onBlur={() => clearErrors("email")}
               placeholder="Email"
               tabIndex={2}
             />
@@ -27,6 +67,12 @@ const Registration: FC = () => {
             <input
               type="text"
               className={cl["registration__input"]}
+              {...register("password", {
+                required: true,
+                validate: RegValidation.password,
+              })}
+              style={{ boxShadow: errors.password ? "0px 0px 5px red" : "" }}
+              onBlur={() => clearErrors("password")}
               placeholder="Password"
               tabIndex={3}
             />
@@ -35,14 +81,32 @@ const Registration: FC = () => {
             <input
               type="text"
               className={cl["registration__input"]}
+              {...register("passportNumber", {
+                required: true,
+                validate: RegValidation.passportNumber,
+              })}
+              style={{
+                boxShadow: errors.passportNumber ? "0px 0px 5px red" : "",
+              }}
+              onBlur={() => clearErrors("passportNumber")}
               placeholder="Passport number"
               tabIndex={4}
             />
           </fieldset>
           <fieldset className={cl["registration__section"]}>
-            <input
+            <InputMask
               type="text"
               className={cl["registration__input"]}
+              mask="+38 (___) ___-__-__"
+              replacement={{ _: /\d/ }}
+              {...register("contactInfo", {
+                required: true,
+                validate: RegValidation.myContacts,
+              })}
+              style={{
+                boxShadow: errors.contactInfo ? "0px 0px 5px red" : "",
+              }}
+              onBlur={() => clearErrors("contactInfo")}
               placeholder="Tel"
               tabIndex={5}
             />
---
+++ src/validation/RegValidation.ts
@@ -0,0 +1,23 @@
+class RegValidation {
+  public static fullname(value: string): boolean {
+    return value.length > 1 ? true : false;
+  }
+  public static email(value: string): boolean {
+    const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(value);
+  }
+  public static password(value: string): boolean {
+    const passwordRegex: RegExp =
+      /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,50}/;
+    return passwordRegex.test(value);
+  }
+  public static passportNumber(value: string): boolean {
+    return value.length > 1 ? true : false;
+  }
+  public static myContacts(value: string): boolean {
+    const phoneNumberRegex: RegExp = /^(?:\+?38|38)?\d{12}$/;
+    return phoneNumberRegex.test(value);
+  }
+}
+
+export default RegValidation;
