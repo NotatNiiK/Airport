43321348f7b42f2ef942179bc55fa63ffe8571cc
Погадайко Антон <pogadajko52@gmail.com>
2023-10-30 20:12:31 +0200

    add route protection

--- src/router/PageRouter.tsx
+++ src/router/PageRouter.tsx
@@ -1,9 +1,22 @@
-import { FC } from "react";
-
-import { Navigate, Routes, Route } from "react-router-dom";
+import { FC, useEffect } from "react";
+import { Navigate, Routes, Route, useNavigate } from "react-router-dom";
 import routes from ".";
 
 const PageRouter: FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isToken()) {
+      navigate("/");
+      return;
+    }
+    navigate("/registration");
+  }, []);
+
+  function isToken(): string | null {
+    return localStorage.getItem("token");
+  }
+
   return (
     <Routes>
       {routes.map((route, index) => {
