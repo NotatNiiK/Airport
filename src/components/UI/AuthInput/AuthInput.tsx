ef2db3be101cc058e3be90a3149cca8e895aae01
Погадайко Антон <pogadajko52@gmail.com>
2023-10-30 21:03:49 +0200

    add forwardRef to AuthInput

--- src/components/UI/AuthInput/AuthInput.tsx
+++ src/components/UI/AuthInput/AuthInput.tsx
@@ -1,10 +1,14 @@
-import { FC, InputHTMLAttributes } from "react";
+import { FC, InputHTMLAttributes, forwardRef, Ref } from "react";
 import cl from "./AuthInput.module.scss";
 
 interface AuthInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const AuthInput: FC<AuthInputProps> = (props) => {
-  return <input type="text" className={cl["auth-input"]} {...props} />;
-};
+const AuthInput: FC<AuthInputProps> = forwardRef(
+  (props, ref: Ref<HTMLInputElement>) => {
+    return (
+      <input type="text" className={cl["auth-input"]} ref={ref} {...props} />
+    );
+  }
+);
 
 export default AuthInput;
