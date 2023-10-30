7f514401e9b4b35c7ec5c3b39fd40fa38498e42a
Погадайко Антон <pogadajko52@gmail.com>
2023-10-30 20:54:18 +0200

    add render array-based links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { NavLink } from "react-router-dom";
 import cl from "./Header.module.scss";
+import routes from "../../router";
 
 const Header: FC = () => {
   return (
@@ -9,16 +10,13 @@ const Header: FC = () => {
         <h2 className={cl["header__title"]}>Airport</h2>
         <nav className="header__nav nav">
           <ul className={cl["nav__list"]}>
-            <li className={cl["nav__item"]}>
-              <NavLink to="/" className={cl["nav__link"]}>
-                Home
-              </NavLink>
-            </li>
-            <li className={cl["nav__item"]}>
-              <NavLink to="/registration" className={cl["nav__link"]}>
-                Registration
-              </NavLink>
-            </li>
+            {routes.map((route) => (
+              <li className={cl["nav__item"]} key={route.id}>
+                <NavLink to={route.path} className={cl["nav__link"]}>
+                  {route.text}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
