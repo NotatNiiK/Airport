df713fdade9c1020745956a07f2046df1ae0c87d
Погадайко Антон <pogadajko52@gmail.com>
2023-10-30 20:54:48 +0200

    update routes array

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,22 @@
 import Home from "../pages/Home/Home";
 import Registration from "../pages/Registration/Registration";
+import Authorization from "../pages/Authorization/Authorization";
 import type { Routes } from "../models/routes";
 
 const routes: Routes = [
-  { id: 0, path: "/", component: Home },
-  { id: 1, path: "/registration", component: Registration },
+  { id: 0, path: "/", text: "Home", component: Home },
+  {
+    id: 1,
+    path: "/authorization",
+    text: "Authorization",
+    component: Authorization,
+  },
+  {
+    id: 2,
+    path: "/registration",
+    text: "Registration",
+    component: Registration,
+  },
 ];
 
 export default routes;
