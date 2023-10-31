8d714b9a53d9a7cdcafcd436f854b27311e4816d
Погадайко Антон <pogadajko52@gmail.com>
2023-10-31 15:06:09 +0200

    add logout button in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,27 @@
 import { FC } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import cl from "./Header.module.scss";
 import routes from "../../router";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AuthService from "../../services/AuthService";
 
 const setActiveLink = ({ isActive }: { isActive: boolean }): string => {
   return isActive ? `${cl["nav__link"]} ${cl["active-link"]}` : cl["nav__link"];
 };
 
 const Header: FC = () => {
+  const navigate = useNavigate();
+
+  function performLogout(): void {
+    AuthService.logout();
+    navigate("/authorization");
+  }
+
   return (
     <header className={cl["header"]}>
       <div className={cl["header__container"]}>
         <h2 className={cl["header__title"]}>Airport</h2>
-        <nav className="header__nav nav">
+        <nav className={cl["nav"]}>
           <ul className={cl["nav__list"]}>
             {routes.map((route) => (
               <li className={cl["nav__item"]} key={route.id}>
@@ -22,6 +31,7 @@ const Header: FC = () => {
               </li>
             ))}
           </ul>
+          <LogoutIcon onClick={performLogout} className={cl["nav__logout"]} />
         </nav>
       </div>
     </header>
