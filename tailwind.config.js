74cae91cbaac36fa1aeebcd4e8a8725690ec2417
Погадайко Антон <pogadajko52@gmail.com>
2023-10-31 22:36:42 +0200

    add custom styles into tailwindConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "auth-bg-color": "#093545",
+        "auth-input-color": "#224957",
+        "auth-btn-color": "#20df7f",
+      },
+      boxShadow: {
+        "auth-btn": "0px 4px 4px 0px rgba(0, 0, 0, 0.3)",
+        "auth-input": "0px 0px 5px rgba(255,255,255, 0.6)",
+      },
+      borderColor: {
+        "auth-btn": "#20df7f",
+      },
+    },
   },
   plugins: [],
 };
