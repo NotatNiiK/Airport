c95123f730c18411c33355cd9e5d2c3e0455ea82
Погадайко Антон <pogadajko52@gmail.com>
2023-11-01 19:39:01 +0200

    add portal to modal component

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import cl from "./Modal.module.scss";
+import { createPortal } from "react-dom";
 
 interface ModalProps {
   visible: boolean;
@@ -17,16 +18,21 @@ const Modal: FC<ModalProps> = ({ visible, toggleModalActive, children }) => {
   }
 
   return (
-    <div className={rootClass.join(" ")} onClick={toggleModalActive}>
-      <div className={cl["modal__wrapper"]}>
-        <div
-          className={modalClass.join(" ")}
-          onClick={(e) => e.stopPropagation()}
-        >
-          {children}
-        </div>
-      </div>
-    </div>
+    <>
+      {createPortal(
+        <div className={rootClass.join(" ")} onClick={toggleModalActive}>
+          <div className={cl["modal__wrapper"]}>
+            <div
+              className={modalClass.join(" ")}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {children}
+            </div>
+          </div>
+        </div>,
+        document.body
+      )}
+    </>
   );
 };
 
