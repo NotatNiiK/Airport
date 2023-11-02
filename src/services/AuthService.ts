361ba529aaf071d8cbab77aa8632ef7bb923e38d
Погадайко Антон <pogadajko52@gmail.com>
2023-11-02 19:17:15 +0200

    fix registration function param name in AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -4,18 +4,11 @@ import { IRegData, IAuthResponse, IAuthData } from "../models/auth";
 
 class AuthService {
   public static registration(
-    registrationData: IRegData
+    regData: IRegData
   ): Promise<AxiosResponse<IAuthResponse>> {
     return axios.post<IAuthResponse>(
       `${process.env.REACT_APP_SERVER_URL}user/logon`,
-      registrationData,
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiLQrtGA0ZbQuSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk4NjA4NDA5LCJleHAiOjE3MDYzODQ0MDl9.q9WK0-NvgBvBsjj-FNuRo48yMYIUKKgqUQkvIYr3bSc",
-        },
-        withCredentials: true,
-      }
+      regData
     );
   }
   public static authorization(
@@ -23,14 +16,7 @@ class AuthService {
   ): Promise<AxiosResponse<IAuthResponse>> {
     return axios.post<IAuthResponse>(
       `${process.env.REACT_APP_SERVER_URL}user/login`,
-      authData,
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiLQrtGA0ZbQuSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk4NjA4NDA5LCJleHAiOjE3MDYzODQ0MDl9.q9WK0-NvgBvBsjj-FNuRo48yMYIUKKgqUQkvIYr3bSc",
-        },
-        withCredentials: true,
-      }
+      authData
     );
   }
 }
