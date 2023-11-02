83982ac427c228f4b593adc367b6130de8a57420
Погадайко Антон <pogadajko52@gmail.com>
2023-11-02 19:36:14 +0200

    add deleteFlight function from service to store

--- src/store/FlightsStore.ts
+++ src/store/FlightsStore.ts
@@ -43,6 +43,26 @@ class AuthStore {
       };
     }
   }
+
+  async deleteFlight(
+    id: number
+  ): Promise<{ hasError: boolean; response: string }> {
+    try {
+      const {
+        data: { success },
+      } = await FlightsService.deleteFlight(id);
+      return {
+        hasError: false,
+        response: success,
+      };
+    } catch (e: any) {
+      console.log(e);
+      return {
+        hasError: true,
+        response: e?.response?.data?.message || "Unexpected error",
+      };
+    }
+  }
 }
 
 export default new AuthStore();
