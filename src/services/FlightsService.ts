a0c2c919943db952026c13a70052a42d25ed185f
Погадайко Антон <pogadajko52@gmail.com>
2023-11-02 15:50:52 +0200

    add headers to FlightsService

--- src/services/FlightsService.ts
+++ src/services/FlightsService.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
 import { AxiosResponse } from "axios";
 import { IFlightsResponse } from "../models/flights";
+import { ICreateData } from "../models/flights";
 
 class FlightsService {
-  public static async createFlight(): Promise<AxiosResponse<IFlightsResponse>> {
-    return axios.get(`${process.env.REACT_APP_SERVER_URL}flight/create`);
+  public static async createFlight(
+    flight: ICreateData
+  ): Promise<AxiosResponse<IFlightsResponse>> {
+    return axios.post(
+      `${process.env.REACT_APP_SERVER_URL}flight/create`,
+      flight,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiLQrtGA0ZbQuSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk4NjA4NDA5LCJleHAiOjE3MDYzODQ0MDl9.q9WK0-NvgBvBsjj-FNuRo48yMYIUKKgqUQkvIYr3bSc",
+        },
+        withCredentials: true,
+      }
+    );
   }
 }
 
