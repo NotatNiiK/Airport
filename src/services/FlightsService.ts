257365caa078bef7a2dd51b7925422320b0fc47e
Погадайко Антон <pogadajko52@gmail.com>
2023-11-02 18:55:39 +0200

    add custom axios instance to FlightsService

--- src/services/FlightsService.ts
+++ src/services/FlightsService.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import $axios from "../http";
 import { AxiosResponse } from "axios";
 import { IFlightsResponse } from "../models/flights";
 import { ICreateData } from "../models/flights";
@@ -7,17 +7,10 @@ class FlightsService {
   public static async createFlight(
     flight: ICreateData
   ): Promise<AxiosResponse<IFlightsResponse>> {
-    return axios.post(
-      `${process.env.REACT_APP_SERVER_URL}flight/create`,
-      flight,
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiLQrtGA0ZbQuSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk4NjA4NDA5LCJleHAiOjE3MDYzODQ0MDl9.q9WK0-NvgBvBsjj-FNuRo48yMYIUKKgqUQkvIYr3bSc",
-        },
-        withCredentials: true,
-      }
-    );
+    return $axios.post(`flight/create`, flight);
+  }
+  public static async getFlights(): Promise<AxiosResponse<any>> {
+    return $axios.get(`flight/get/all`);
   }
 }
 
