98156e838a26686ab7c1dfcb0579b9ab661aee65
Погадайко Антон <pogadajko52@gmail.com>
2023-11-02 15:30:21 +0200

    add FlightValidation class

---
+++ src/validation/FlightValidation.ts
@@ -0,0 +1,18 @@
+class FlightValidation {
+  static departureTimeValue: Date = new Date();
+  public static required(value: string): boolean {
+    return value.length > 1 ? true : false;
+  }
+  public static departureTime(value: string): boolean {
+    FlightValidation.departureTimeValue = new Date(value);
+    return new Date(value) >= new Date();
+  }
+  public static arrivalTime(value: string): boolean {
+    return (
+      new Date(value) >= new Date() &&
+      new Date(value) > FlightValidation.departureTimeValue
+    );
+  }
+}
+
+export default FlightValidation;
