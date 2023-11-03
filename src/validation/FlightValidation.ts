8a472c96e61ae81b58823282afb678958361ce70
Погадайко Антон <pogadajko52@gmail.com>
2023-11-03 12:52:22 +0200

    removed the unnecessary check

--- src/validation/FlightValidation.ts
+++ src/validation/FlightValidation.ts
@@ -1,18 +1,19 @@
 class FlightValidation {
   static departureTimeValue: Date = new Date();
+
   public static required(value: string): boolean {
     return value.length > 1 ? true : false;
   }
+
   public static departureTime(value: string): boolean {
     FlightValidation.departureTimeValue = new Date(value);
     return new Date(value) >= new Date();
   }
+
   public static arrivalTime(value: string): boolean {
-    return (
-      new Date(value) >= new Date() &&
-      new Date(value) > FlightValidation.departureTimeValue
-    );
+    return new Date(value) > FlightValidation.departureTimeValue;
   }
+
   public static isActive(value: string): string {
     return value;
   }
