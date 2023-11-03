0f7ec39ec1148172b641f4fe4b3bbd9db04b1d88
Погадайко Антон <pogadajko52@gmail.com>
2023-11-03 14:41:45 +0200

    create data folder and add there navLinks array

---
+++ src/data/navlinks.ts
@@ -0,0 +1,17 @@
+import { INavLinks } from "../models/routes";
+
+const navLinks: INavLinks = [
+  { id: 0, path: "/", text: "Home" },
+  {
+    id: 1,
+    path: "/login",
+    text: "Login",
+  },
+  {
+    id: 2,
+    path: "/signin",
+    text: "Sign in",
+  },
+];
+
+export default navLinks;
