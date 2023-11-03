aa9bdfefefdca8c403dc4cbbf916c3f5926d1c77
Погадайко Антон <pogadajko52@gmail.com>
2023-11-03 15:59:28 +0200

    open tickets page while click at flight

--- src/components/FlightsList/FightsList.tsx
+++ src/components/FlightsList/FightsList.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import cl from "./FlightsList.module.scss";
 import FlightItem from "../FlightsItem/FlightsItem";
 import { IFlight } from "../../models/flights";
+import { Link } from "react-router-dom";
 import Loader from "../UI/Loader/Loader";
 
 interface FlightsListProps {
@@ -17,7 +18,9 @@ const FlightsList: FC<FlightsListProps> = ({ flights, isLoading }) => {
   return flights.length > 0 ? (
     <ul className={cl["flights-list"]}>
       {flights.map((flight: IFlight) => (
-        <FlightItem flight={flight} key={flight.id} />
+        <Link to={`/tickets/${flight.id}`}>
+          <FlightItem flight={flight} key={flight.id} />
+        </Link>
       ))}
     </ul>
   ) : (
