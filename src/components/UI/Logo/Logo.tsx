8d023cdd0ac669735f46b0da93425884507e51ef
Погадайко Антон <pogadajko52@gmail.com>
2023-11-03 15:14:28 +0200

    create Logo UI component

---
+++ src/components/UI/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import cl from "./Logo.module.scss";
+import LocalAirportIcon from "@mui/icons-material/LocalAirport";
+
+const Logo: FC = () => {
+  return (
+    <h2 className={cl["logo"]}>
+      Utravel <LocalAirportIcon />{" "}
+    </h2>
+  );
+};
+
+export default Logo;
