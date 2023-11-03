5b4f4bf56a00d66eda42f59ccdd6b44a963d8831
Погадайко Антон <pogadajko52@gmail.com>
2023-11-03 15:14:46 +0200

    create array of social media

---
+++ src/data/socialMedia.ts
@@ -0,0 +1,15 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+const socialMedia = [
+  { id: 0, path: "https://github.com/NotatNiiK", icon: GitHubIcon },
+  { id: 1, path: "https://t.me/antoniobanderra", icon: TelegramIcon },
+  {
+    id: 2,
+    path: "https://www.linkedin.com/in/anton-pogadaiko",
+    icon: LinkedInIcon,
+  },
+];
+
+export default socialMedia;
