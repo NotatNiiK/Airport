1e85a66714151537bbfb1262b76c64f225ef1469
Погадайко Антон <pogadajko52@gmail.com>
2023-11-03 19:51:43 +0200

    add usePasswordMask for toggle password input

---
+++ src/hooks/usePasswordMask.ts
@@ -0,0 +1,14 @@
+import { useState, useMemo } from "react";
+
+export const usePasswordMask = (): [boolean, string, () => void] => {
+  const [isPasswordMask, setIsPasswordMask] = useState<boolean>(false);
+
+  const passwordType = useMemo((): string => {
+    return isPasswordMask ? "password" : "text";
+  }, [isPasswordMask]);
+
+  function togglePasswordMask(): void {
+    setIsPasswordMask(!isPasswordMask);
+  }
+  return [isPasswordMask, passwordType, togglePasswordMask];
+};
