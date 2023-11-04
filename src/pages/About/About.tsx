b0140f693214ab6703bf2f477608018594400e28
Погадайко Антон <pogadajko52@gmail.com>
2023-11-04 21:26:52 +0200

    add about page styles

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,50 @@
 import { FC } from "react";
+import cl from "./About.module.scss";
+import Header from "../../components/Header/Header";
+import PlainImage from "../../assets/Pages/About/Plain.png";
 
 const About: FC = () => {
-  return <h1>About page</h1>;
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <section className={cl["about"]}>
+          <div className={cl["about__container"]}>
+            <div className={cl["about__content"]}>
+              <h1 className={cl["about__title"]}>Utravel airport</h1>
+              <p className={cl["about__paragraph"]}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+                voluptate tenetur eum iste id eos non, quis nesciunt, illum
+                placeat eveniet quidem suscipit officia temporibus perspiciatis
+                nostrum nemo et dolore.
+              </p>
+              <p className={cl["about__paragraph"]}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+                voluptate tenetur eum iste id eos non, quis nesciunt, illum
+                placeat eveniet quidem suscipit officia temporibus perspiciatis
+                nostrum nemo et dolore.
+              </p>
+              <p className={cl["about__paragraph"]}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+                voluptate tenetur eum iste id eos non, quis nesciunt, illum
+                placeat eveniet quidem suscipit officia temporibus perspiciatis
+                nostrum nemo et dolore.
+              </p>
+              <p className={cl["about__paragraph"]}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+                voluptate tenetur eum iste id eos non, quis nesciunt, illum
+                placeat eveniet quidem suscipit officia temporibus perspiciatis
+                nostrum nemo et dolore.
+              </p>
+            </div>
+            <div className={cl["about__image"]}>
+              <img src={PlainImage} alt="Plain" />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default About;
