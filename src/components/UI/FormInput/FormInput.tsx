ea953b12bbef25c5b1af4855ac368c86547e3eda
Погадайко Антон <pogadajko52@gmail.com>
2023-11-04 17:13:21 +0200

    removed inline styles from general form

--- src/components/UI/FormInput/FormInput.tsx
+++ src/components/UI/FormInput/FormInput.tsx
@@ -1,11 +1,22 @@
 import { FC, InputHTMLAttributes, forwardRef, Ref } from "react";
 import cl from "./FormInput.module.scss";
+import { FieldError } from "react-hook-form";
 
-interface AuthInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface AuthInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isError: FieldError | undefined;
+}
 
 const FormInput: FC<AuthInputProps> = forwardRef(
-  (props, ref: Ref<HTMLInputElement>) => {
-    return <input className={cl["form-input"]} ref={ref} {...props} />;
+  ({ isError, ...props }, ref: Ref<HTMLInputElement>) => {
+    return (
+      <input
+        className={`${cl["form-input"]} ${
+          isError ? cl["validation-error"] : ""
+        }`}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 
--- src/components/forms/GeneralFlightForm/GeneralFlightForm.tsx
+++ src/components/forms/GeneralFlightForm/GeneralFlightForm.tsx
@@ -87,11 +87,11 @@ const GeneralFlightForm: FC<GeneralFlightFormProps> = ({
       <h2 className={cl["general-form__title"]}>{title}</h2>
       <section className={cl["general-form__section"]}>
         <FormInput
+          isError={errors.flightNumber}
           {...register("flightNumber", {
             required: true,
             validate: FlightValidation.required,
           })}
-          style={{ boxShadow: errors.flightNumber ? "0px 0px 7px red" : "" }}
           onBlur={() => clearErrors("flightNumber")}
           placeholder="Flight number"
           tabIndex={1}
@@ -99,13 +99,11 @@ const GeneralFlightForm: FC<GeneralFlightFormProps> = ({
       </section>
       <section className={cl["general-form__section"]}>
         <FormInput
+          isError={errors.departureLocation}
           {...register("departureLocation", {
             required: true,
             validate: FlightValidation.required,
           })}
-          style={{
-            boxShadow: errors.departureLocation ? "0px 0px 7px red" : "",
-          }}
           onBlur={() => clearErrors("departureLocation")}
           placeholder="Departure location"
           tabIndex={2}
@@ -113,13 +111,11 @@ const GeneralFlightForm: FC<GeneralFlightFormProps> = ({
       </section>
       <section className={cl["general-form__section"]}>
         <FormInput
+          isError={errors.destination}
           {...register("destination", {
             required: true,
             validate: FlightValidation.required,
           })}
-          style={{
-            boxShadow: errors.destination ? "0px 0px 7px red" : "",
-          }}
           onBlur={() => clearErrors("destination")}
           placeholder="Destination"
           tabIndex={3}
@@ -127,13 +123,11 @@ const GeneralFlightForm: FC<GeneralFlightFormProps> = ({
       </section>
       <section className={cl["general-form__section"]}>
         <FormInput
+          isError={errors.departureTime}
           {...register("departureTime", {
             required: true,
             validate: FlightValidation.departureTime,
           })}
-          style={{
-            boxShadow: errors.departureTime ? "0px 0px 7px red" : "",
-          }}
           onBlur={() => clearErrors("departureTime")}
           type="datetime-local"
           placeholder="Departure time"
@@ -147,13 +141,11 @@ const GeneralFlightForm: FC<GeneralFlightFormProps> = ({
         ].join(" ")}
       >
         <FormInput
+          isError={errors.arrivalTime}
           {...register("arrivalTime", {
             required: true,
             validate: FlightValidation.arrivalTime,
           })}
-          style={{
-            boxShadow: errors.arrivalTime ? "0px 0px 7px red" : "",
-          }}
           onBlur={() => clearErrors("arrivalTime")}
           type="datetime-local"
           placeholder="Arrival time"
@@ -167,10 +159,9 @@ const GeneralFlightForm: FC<GeneralFlightFormProps> = ({
         ].join(" ")}
       >
         <span
-          className={cl["general-form__label"]}
-          style={{
-            color: errors.flightStatus ? "red" : "",
-          }}
+          className={`${cl["general-form__label"]} ${
+            errors.flightStatus ? "text-red-600" : ""
+          }`}
         >
           Is flight status active?
         </span>{" "}
