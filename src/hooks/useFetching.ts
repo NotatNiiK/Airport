c883d40a4965b85c6b38fd044463cc8430c55749
Погадайко Антон <pogadajko52@gmail.com>
2023-11-10 22:58:22 +0200

    improved useFetching hook typing

--- src/hooks/useFetching.ts
+++ src/hooks/useFetching.ts
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
-export const useFetching = (callback: (...props: any) => Promise<any>): any => {
+export const useFetching = (
+  callback: (...args: any[]) => Promise<any>
+): [(...data: any[]) => Promise<any>, boolean] => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetching = async (data: any) => {
+  const fetching = async (...data: any[]): Promise<any> => {
     try {
       setIsLoading(true);
       await callback(data);
