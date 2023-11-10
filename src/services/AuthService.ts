a8a40652ba0b1ca8169cc84a7c290fa2916e1ae9
Погадайко Антон <pogadajko52@gmail.com>
2023-11-10 23:54:20 +0200

    refactored authService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -3,23 +3,23 @@ import type { AxiosResponse } from "axios";
 import { IRegData, IAuthResponse, IAuthData } from "../models/auth";
 
 class AuthService {
-  private static async authRequest<T>(
+  private static async authRequest(
     url: string,
     data: IRegData | IAuthData
-  ): Promise<AxiosResponse<T>> {
-    return axios.post<T>(`${process.env.REACT_APP_SERVER_URL}${url}`, data);
+  ): Promise<AxiosResponse<IAuthResponse>> {
+    return axios.post(`${process.env.REACT_APP_SERVER_URL}${url}`, data);
   }
 
   public static async registration(
     regData: IRegData
   ): Promise<AxiosResponse<IAuthResponse>> {
-    return AuthService.authRequest<IAuthResponse>("user/logon", regData);
+    return AuthService.authRequest("user/logon", regData);
   }
 
   public static async authorization(
     authData: IAuthData
   ): Promise<AxiosResponse<IAuthResponse>> {
-    return AuthService.authRequest<IAuthResponse>("user/login", authData);
+    return AuthService.authRequest("user/login", authData);
   }
 }
 
