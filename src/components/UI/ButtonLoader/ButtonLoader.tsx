0d212fce9b8d083ebce477a85705472a73b6d4f9
Погадайко Антон <pogadajko52@gmail.com>
2023-11-11 11:08:16 +0200

    renamed class loader to button-loader

--- src/components/UI/ButtonLoader/ButtonLoader.tsx
+++ src/components/UI/ButtonLoader/ButtonLoader.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import cl from "./ButtonLoader.module.scss";
 
 const ButtonLoader: FC = () => {
-  return <span className={cl["loader"]}></span>;
+  return <span className={cl["button-loader"]}></span>;
 };
 
 export default ButtonLoader;
