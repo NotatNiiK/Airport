f1f435897d0190c4d375af69a9bc6f3b9a04caae
Погадайко Антон <pogadajko52@gmail.com>
2023-11-11 17:30:42 +0200

    added save tokenInfo from localStorage to AuthStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { IToken } from "./models/token";
 import PageRouter from "./router/PageRouter";
 import isUserAuth from "./utils/isUserAuth";
 import AuthStore from "./store/AuthStore";
 
 const App: FC = () => {
-  if (isUserAuth()) AuthStore.setIsAuth(true);
+  useEffect((): void => {
+    if (isUserAuth()) AuthStore.setIsAuth(true);
+
+    const tokenInfo = localStorage.getItem("tokenInfo");
+    if (tokenInfo) {
+      const parsedTokenInfo: IToken = JSON.parse(tokenInfo);
+      AuthStore.setTokenInfo(parsedTokenInfo);
+    }
+  }, []);
 
   return (
     <div className="wrapper">
