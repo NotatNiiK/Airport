9b59c68638a945b5e5b005f5a68d284e4b7c2343
Погадайко Антон <pogadajko52@gmail.com>
2023-11-11 15:35:08 +0200

    added isError props to select ui component

--- src/components/UI/TicketSelect/TicketSelect.tsx
+++ src/components/UI/TicketSelect/TicketSelect.tsx
@@ -1,21 +1,33 @@
 import { FC, SelectHTMLAttributes, forwardRef, Ref } from "react";
+import { FieldError } from "react-hook-form";
 import { IOptions } from "../../../models/options";
 import cl from "./TicketSelect.module.scss";
 
 interface TicketSelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   options: IOptions;
   defaultValue: string;
+  isError?: FieldError;
 }
 
 const TicketSelect: FC<TicketSelectProps> = forwardRef(
-  ({ options, defaultValue, ...props }, ref: Ref<HTMLSelectElement>) => {
+  (
+    { options, defaultValue, isError, ...props },
+    ref: Ref<HTMLSelectElement>
+  ) => {
+    const ticketSelectClasses: string = [
+      cl["ticket-select"],
+      isError ? cl["validation-error"] : "",
+    ].join(" ");
+
     return (
-      <select className={cl["ticket-select"]} {...props} ref={ref}>
+      <select className={ticketSelectClasses} {...props} ref={ref}>
         <option value="" disabled>
           {defaultValue}
         </option>
         {options.map(({ text, value }) => (
-          <option value={value}>{text}</option>
+          <option value={value} key={text}>
+            {text}
+          </option>
         ))}
       </select>
     );
