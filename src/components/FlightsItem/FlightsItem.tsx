6ad0986dc9fc4c60402257da56f89cf7a6c8a3c4
Погадайко Антон <pogadajko52@gmail.com>
2023-11-11 13:03:31 +0200

    added delete error handling to FlightItem and refacroted it

--- src/components/FlightsItem/FlightsItem.tsx
+++ src/components/FlightsItem/FlightsItem.tsx
@@ -1,42 +1,54 @@
 import { FC, useState, useEffect } from "react";
-import cl from "./FlightsItem.module.scss";
-import FlightTakeoffTwoToneIcon from "@mui/icons-material/FlightTakeoffTwoTone";
+import { Link } from "react-router-dom";
 import { IFlight } from "../../models/flights";
+import { IToken } from "../../models/token";
 import { useFetching } from "../../hooks/useFetching";
-import FlightsStore from "../../store/FlightStore";
-import AuthStore from "../../store/AuthStore";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import Modal from "../UI/Modal/Modal";
+import { useModal } from "../../hooks/useModal";
+import { useAlert } from "../../hooks/useAlert";
+import cl from "./FlightsItem.module.scss";
 import GeneralFlightForm from "../forms/GeneralFlightForm/GeneralFlightForm";
 import ConfirmForm from "../forms/ConfirmForm/ConfirmForm";
-import { Link } from "react-router-dom";
-import { IToken } from "../../models/token";
+import ErrorAlert from "../ErrorAlert/ErrorAlert";
+import Modal from "../UI/Modal/Modal";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import FlightTakeoffTwoToneIcon from "@mui/icons-material/FlightTakeoffTwoTone";
+import AuthStore from "../../store/AuthStore";
+import FlightsStore from "../../store/FlightStore";
+import goToTicketPage from "../../utils/goToTicketPage";
 
 interface FlightItemProps {
   flight: IFlight;
 }
 
 const FlightItem: FC<FlightItemProps> = ({ flight }) => {
-  const [isEditModalOpen, setIsEditModal] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModal] = useState(false);
+  const [isEditModalOpen, toggleEditModal] = useModal();
+  const [isDeleteModalOpen, toggleDeleteModal] = useModal();
+  const [errorAlert, showAlert] = useAlert();
   const [userId, setUserId] = useState<number>(0);
 
-  function toggleEditModal(): void {
-    setIsEditModal(!isEditModalOpen);
-  }
+  const deleteIconClasses: string = [
+    cl["flights-item__button"],
+    cl["flights-item__button_delete"],
+  ].join(" ");
 
-  const [deleteFlight, isLoading] = useFetching(async (id: number) => {
-    const r = await FlightsStore.deleteFlight(id);
-    console.log(r.response);
-    await FlightsStore.getFlights();
-  });
+  const editIconClasses: string = [
+    cl["flights-item__button"],
+    cl["flights-item__button_edit"],
+  ].join(" ");
 
-  function toggleDeleteModal(): void {
-    setIsDeleteModal(!isDeleteModalOpen);
-  }
+  const [deleteFlight, isLoading] = useFetching(
+    async (id: number): Promise<void> => {
+      const deleteResponse = await FlightsStore.deleteFlight(id);
+      if (deleteResponse.hasError) {
+        showAlert(deleteResponse.response);
+        return;
+      }
+      await FlightsStore.getFlights();
+    }
+  );
 
-  useEffect(() => {
+  useEffect((): void => {
     const tokenInfo = localStorage.getItem("tokenInfo");
 
     if (tokenInfo) {
@@ -61,9 +73,9 @@ const FlightItem: FC<FlightItemProps> = ({ flight }) => {
           Flight number: {flight.flightNumber}
         </p>
         <p className={cl["flights-item__price"]}>{flight.price}$</p>
-        <div className={[cl["flights-item__buttons"]].join(" ")}>
+        <div className={cl["flights-item__buttons"]}>
           <Link
-            to={`tickets/${flight.id}/${flight.flightNumber}/${flight.price}/${userId}`}
+            to={goToTicketPage(flight, userId)}
             className={cl["flights-item__buy-ticket"]}
           >
             Buy ticket
@@ -71,28 +83,18 @@ const FlightItem: FC<FlightItemProps> = ({ flight }) => {
           {AuthStore.isAdmin && (
             <>
               <DeleteIcon
-                onClick={() => {
-                  setIsDeleteModal(true);
-                }}
+                onClick={toggleDeleteModal}
+                className={deleteIconClasses}
                 sx={{
                   fontSize: "30px",
                 }}
-                className={[
-                  cl["flights-item__button"],
-                  cl["flights-item__button_delete"],
-                ].join(" ")}
               />
               <EditIcon
-                onClick={() => {
-                  setIsEditModal(true);
-                }}
+                onClick={toggleEditModal}
+                className={editIconClasses}
                 sx={{
                   fontSize: "30px",
                 }}
-                className={[
-                  cl["flights-item__button"],
-                  cl["flights-item__button_edit"],
-                ].join(" ")}
               />
             </>
           )}
@@ -115,6 +117,7 @@ const FlightItem: FC<FlightItemProps> = ({ flight }) => {
           title="Do you really want to delete this flight?"
         />
       </Modal>
+      <ErrorAlert isError={errorAlert.error} message={errorAlert.message} />
     </li>
   );
 };
