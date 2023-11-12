bd9ed483d30692af670a56ce86365824bbfed63c
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 13:29:40 +0200

    added NotFound UI component

---
+++ src/components/UI/NotFound/NotFound.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from "react";
+import cl from "./NotFound.module.scss";
+import BlockIcon from "@mui/icons-material/Block";
+
+interface NotFoundProps {
+  children: ReactNode;
+  colorClass: string;
+}
+
+const NotFound: FC<NotFoundProps> = ({ children, colorClass }) => {
+  const rootClasses: string = [cl["not-found"], colorClass].join(" ");
+
+  return (
+    <div className={rootClasses}>
+      <BlockIcon
+        sx={{
+          fontSize: "120px",
+        }}
+      />
+      <h2 className={cl["not-found__title"]}>{children}</h2>
+    </div>
+  );
+};
+
+export default NotFound;
