50b6033e82edf5735e9d421eb68041dd894a4779
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 13:35:45 +0200

    ready TicketList component

--- src/components/TicketsList/TicketsList.tsx
+++ src/components/TicketsList/TicketsList.tsx
@@ -1,31 +1,22 @@
 import { FC } from "react";
 import { ITickets } from "../../models/ticket";
 import cl from "./TicketsList.module.scss";
-import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
+import NotFound from "../UI/NotFound/NotFound";
+import TicketItem from "../TicketItem/TicketItem";
 
 interface TicketsListProps {
   ticketsList: ITickets;
 }
 
 const TicketsList: FC<TicketsListProps> = ({ ticketsList }) => {
-  return (
+  return ticketsList.length > 0 ? (
     <ul className={cl["ticket-list"]}>
-      {ticketsList.map((ticket) => (
-        <li className={cl["ticket-item"]} key={ticket.id}>
-          <div className={cl["ticket-item__content"]}>
-            <h2 className={cl["ticket-item__direction"]}>London - Kyiv</h2>
-            <p className={cl["ticket-item__info"]}>
-              Bought: {ticket.purchaseDate}
-            </p>
-            <p className={cl["ticket-item__info"]}>Price: {ticket.cost}</p>
-            <p className={cl["ticket-item__info"]}>Place: {ticket.place}</p>
-          </div>
-          <div className={cl["ticket-item__image"]}>
-            <AirplaneTicketIcon />
-          </div>
-        </li>
+      {ticketsList.map((ticket: any) => (
+        <TicketItem ticket={ticket} />
       ))}
     </ul>
+  ) : (
+    <NotFound colorClass="text-white">There are no tickets</NotFound>
   );
 };
 
