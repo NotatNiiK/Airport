7ec1962d8ff3ada0fc61bf0b7b39103184649a55
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 12:45:15 +0200

    add raw ticket list

--- src/components/TicketsList/TicketsList.tsx
+++ src/components/TicketsList/TicketsList.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { ITickets } from "../../models/ticket";
+import cl from "./TicketsList.module.scss";
+import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
 
 interface TicketsListProps {
   ticketsList: ITickets;
@@ -7,9 +9,21 @@ interface TicketsListProps {
 
 const TicketsList: FC<TicketsListProps> = ({ ticketsList }) => {
   return (
-    <ul>
+    <ul className={cl["ticket-list"]}>
       {ticketsList.map((ticket) => (
-        <li>{ticket.classes}</li>
+        <li className={cl["ticket-item"]} key={ticket.id}>
+          <div className={cl["ticket-item__content"]}>
+            <h2 className={cl["ticket-item__direction"]}>London - Kyiv</h2>
+            <p className={cl["ticket-item__info"]}>
+              Bought: {ticket.purchaseDate}
+            </p>
+            <p className={cl["ticket-item__info"]}>Price: {ticket.cost}</p>
+            <p className={cl["ticket-item__info"]}>Place: {ticket.place}</p>
+          </div>
+          <div className={cl["ticket-item__image"]}>
+            <AirplaneTicketIcon />
+          </div>
+        </li>
       ))}
     </ul>
   );
--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -3,13 +3,13 @@ import cl from "./Account.module.scss";
 import TicketStore from "../../store/TicketStore";
 import AuthStore from "../../store/AuthStore";
 import Header from "../../components/Header/Header";
+import TicketsList from "../../components/TicketsList/TicketsList";
 
 const Account: FC = () => {
   useEffect(() => {
     const fetching = async () => {
       const userId = AuthStore.tokenInfo.id || 0;
-      console.log(userId);
-      const r = await TicketStore.getTickets(userId);
+      await TicketStore.getTickets(userId);
     };
     fetching();
   }, []);
@@ -22,6 +22,7 @@ const Account: FC = () => {
             <h1 className={cl["account__title"]}>
               Welcome, {AuthStore.tokenInfo.username}
             </h1>
+            <TicketsList ticketsList={TicketStore.ticketsList} />
           </div>
         </div>
       </main>
