6eb8958792fbda51db73a10fa00edec7f672a490
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 22:00:39 +0200

    improved formatTicketDate util

--- src/utils/formatTicketDate.ts
+++ src/utils/formatTicketDate.ts
@@ -1,12 +1,17 @@
 function formatTicketDate(): string {
   const currentDate: Date = new Date();
-  const day: number = currentDate.getDate();
-  const month: number = currentDate.getMonth() + 1;
+  const day: string = padZero(currentDate.getDate());
+  const month: string = padZero(currentDate.getMonth() + 1);
   const year: number = currentDate.getFullYear();
-  const hours: number = currentDate.getHours();
-  const minutes: number = currentDate.getMinutes();
+  const hours: string = padZero(currentDate.getHours());
+  const minutes: string = padZero(currentDate.getMinutes());
+
   const formattedDate: string = `${day}.${month}.${year} ${hours}:${minutes}`;
   return formattedDate;
+}
+
+function padZero(value: number): string {
+  return value < 10 ? `0${value}` : `${value}`;
 }
 
 export default formatTicketDate;
