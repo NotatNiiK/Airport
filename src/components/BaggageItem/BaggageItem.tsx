11ed7110183fc093b301495fadc2f077b0a85662
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 21:06:42 +0200

    added edit and delete baggage item

--- src/components/BaggageItem/BaggageItem.tsx
+++ src/components/BaggageItem/BaggageItem.tsx
@@ -1,14 +1,35 @@
 import { FC } from "react";
 import { IBaggage } from "../../models/baggage";
+import { useParams } from "react-router-dom";
 import cl from "./BaggageItem.module.scss";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import Modal from "../UI/Modal/Modal";
+import { useModal } from "../../hooks/useModal";
+import GeneralBaggageForm from "../forms/GeneralBaggageForm/GeneralBaggageForm";
+import ConfirmForm from "../forms/ConfirmForm/ConfirmForm";
+import { useFetching } from "../../hooks/useFetching";
+import BaggageStore from "../../store/BaggageStore";
 
 interface BaggageItemProps {
   baggage: IBaggage;
 }
 
 const BaggageItem: FC<BaggageItemProps> = ({ baggage }) => {
+  const { tiketId } = useParams();
+
+  const [isEditModalOpen, toggleEditModal] = useModal();
+  const [isDeleteModalOpen, toggleDeleteModal] = useModal();
+
+  const [deleteBaggage, isLoading] = useFetching(async (): Promise<void> => {
+    const baggageId = baggage.id || 0;
+    await BaggageStore.deleteBaggage(baggageId);
+    if (tiketId) {
+      const ticketID = +tiketId;
+      await BaggageStore.getBaggageById(ticketID);
+    }
+  });
+
   return (
     <li className={cl["baggage-item"]}>
       <h2 className={cl["baggage-item__cost"]}>Cost: {baggage.cost}$</h2>
@@ -24,13 +45,30 @@ const BaggageItem: FC<BaggageItemProps> = ({ baggage }) => {
         </li>
       </ul>
       <div className="baggage-item__buttons">
-        <button className="baggage-item__button">
+        <button className="baggage-item__button" onClick={toggleEditModal}>
           <EditNoteIcon />
         </button>
-        <button className="baggage-item__button">
+        <button className="baggage-item__button" onClick={toggleDeleteModal}>
           <DeleteForeverIcon />
         </button>
       </div>
+      <Modal open={isEditModalOpen} toggleModal={toggleEditModal}>
+        <GeneralBaggageForm
+          title="Edit Baggage"
+          isEdit={true}
+          isClearForm={false}
+          closeModal={toggleEditModal}
+          baggage={baggage}
+        />
+      </Modal>
+      <Modal open={isDeleteModalOpen} toggleModal={toggleDeleteModal}>
+        <ConfirmForm
+          title="Do you relly want to delete?"
+          closeModal={toggleDeleteModal}
+          performAction={deleteBaggage}
+          loading={isLoading}
+        ></ConfirmForm>
+      </Modal>
     </li>
   );
 };
