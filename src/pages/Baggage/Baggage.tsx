ab22537887d5a0ddc7196d929b59062a2d7862f1
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 18:19:33 +0200

    add init baggage page design and functionals

--- src/pages/Baggage/Baggage.tsx
+++ src/pages/Baggage/Baggage.tsx
@@ -1,7 +1,34 @@
 import { FC } from "react";
+import cl from "./Baggage.module.scss";
+import Header from "../../components/Header/Header";
+import Modal from "../../components/UI/Modal/Modal";
+import { useModal } from "../../hooks/useModal";
 
 const Baggage: FC = () => {
-  return <h1>Baggage</h1>;
+  const [isCreateModalOpen, toggleCreateModal] = useModal();
+
+  return (
+    <>
+      <Header />
+      <div className={cl["baggage"]}>
+        <section className={cl["baggage__container"]}>
+          <div className={cl["baggage__panel"]}>
+            <h1 className={cl["baggage__title"]}>Your Baggage:</h1>
+            <button
+              className={cl["baggage__button"]}
+              tabIndex={1}
+              onClick={toggleCreateModal}
+            >
+              Add baggage +
+            </button>
+          </div>
+        </section>
+        <Modal open={isCreateModalOpen} toggleModal={toggleCreateModal}>
+          Create
+        </Modal>
+      </div>
+    </>
+  );
 };
 
 export default Baggage;
