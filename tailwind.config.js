4f11a7580ff162325243a10d603a424401a0d59d
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 21:29:02 +0200

    renamed custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,14 @@ export default {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
-        "auth-bg-color": "#093545",
-        "auth-input-color": "#224957",
-        "auth-btn-color": "#20df7f",
-      },
       boxShadow: {
-        "auth-btn": "0px 4px 4px 0px rgba(0, 0, 0, 0.3)",
-        "auth-input": "0px 0px 5px rgba(255,255,255, 0.6)",
-      },
-      borderColor: {
-        "auth-btn": "#20df7f",
+        "opacity-black": "0px 4px 4px 0px rgba(0, 0, 0, 0.3)",
+        "opacity-white": "0px 0px 5px rgba(255,255,255, 0.6)",
       },
       colors: {
-        "nav-link": "#20df7f",
-        "custom-green": "#093545",
-      },
-      inset: {
-        hidden: "100%",
-      },
-      translate: {
-        "reverse-half": "-50%",
+        "dark-green": "#093545",
+        "light-dark-green": "#224957",
+        "bright-green": "#20df7f",
       },
     },
   },
