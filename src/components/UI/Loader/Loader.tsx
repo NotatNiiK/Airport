b93b7590ed6bbca08fba36019b850c9943d6adbb
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 14:42:30 +0200

    update Loader component

--- src/components/UI/Loader/Loader.tsx
+++ src/components/UI/Loader/Loader.tsx
@@ -3,17 +3,8 @@ import cl from "./Loader.module.scss";
 
 const Loader: FC = () => {
   return (
-    <div className={cl["loader"]}>
-      <div className={cl["loader-roller"]}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div className={cl["loader-block"]}>
+      <span className={cl["loader"]}></span>
     </div>
   );
 };
