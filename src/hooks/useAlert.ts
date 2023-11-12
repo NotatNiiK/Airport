cb409a33f30a8f9c438cbf18eaf22e0e82fd6837
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 23:26:34 +0200

    renamed variables in useAlert hook

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -2,24 +2,24 @@ import { useState } from "react";
 import { IAlert } from "../models/alert";
 
 export const useAlert = (): [IAlert, (message: string) => void] => {
-  const [errorAlert, setErrorAlert] = useState<IAlert>({
+  const [alert, setAlert] = useState<IAlert>({
     show: false,
     message: "",
   });
 
   function showAlert(message: string): void {
-    setErrorAlert({
+    setAlert({
       show: true,
       message,
     });
 
     setTimeout(() => {
-      setErrorAlert({
+      setAlert({
         show: false,
         message: "",
       });
     }, 3000);
   }
 
-  return [errorAlert, showAlert];
+  return [alert, showAlert];
 };
