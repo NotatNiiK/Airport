c55f28b5aba0a887673c3bb7188108b63587e10b
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 15:29:22 +0200

    added button in TicketItem for adding baggage

--- src/components/TicketItem/TicketItem.tsx
+++ src/components/TicketItem/TicketItem.tsx
@@ -11,15 +11,25 @@ const TicketItem: FC<TicketItemProps> = ({ ticket }) => {
   return (
     <li className={cl["ticket-item"]} key={ticket.id}>
       <div className={cl["ticket-item__content"]}>
-        <h2 className={cl["ticket-item__direction"]}>London - Kyiv</h2>
+        <h2 className={cl["ticket-item__direction"]}>
+          {ticket.depatureLocation} - {ticket.destination}
+        </h2>
         <p className={cl["ticket-item__time"]}>
-          Arrival at: {ticket.purchaseDate}
+          Arrival at: {ticket.arrivalTime}
         </p>
         <ul className={cl["info-list"]}>
           <li className={cl["info-list__item"]}>Price: {ticket.cost}$</li>
           <li className={cl["info-list__item"]}>Class: {ticket.classes}</li>
           <li className={cl["info-list__item"]}>Place: {ticket.place}</li>
         </ul>
+        <div className={cl["ticket-item__baggage"]}>
+          <button
+            className={cl["ticket-item__add-baggage"]}
+            title="Add baggage"
+          >
+            +
+          </button>
+        </div>
       </div>
       <div className={cl["ticket-item__image"]}>
         <AirplaneTicketIcon />
