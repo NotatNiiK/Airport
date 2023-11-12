48766b86b57030cd2af8250a9c8a9b5379c2b0bd
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 20:49:25 +0200

    add design for BaggageItem

--- src/components/BaggageItem/BaggageItem.tsx
+++ src/components/BaggageItem/BaggageItem.tsx
@@ -1,13 +1,38 @@
 import { FC } from "react";
 import { IBaggage } from "../../models/baggage";
 import cl from "./BaggageItem.module.scss";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 interface BaggageItemProps {
   baggage: IBaggage;
 }
 
 const BaggageItem: FC<BaggageItemProps> = ({ baggage }) => {
-  return <div className={cl["baggage-item"]}>{baggage.cost}</div>;
+  return (
+    <li className={cl["baggage-item"]}>
+      <h2 className={cl["baggage-item__cost"]}>Cost: {baggage.cost}$</h2>
+      <ul className={cl["baggage-item__list"]}>
+        <li className={cl["baggage-item__list-item"]}>
+          Weight: {baggage.weight}kg
+        </li>
+        <li className={cl["baggage-item__list-item"]}>
+          Height: {baggage.height}cm
+        </li>
+        <li className={cl["baggage-item__list-item"]}>
+          Width: {baggage.width}cm
+        </li>
+      </ul>
+      <div className="baggage-item__buttons">
+        <button className="baggage-item__button">
+          <EditNoteIcon />
+        </button>
+        <button className="baggage-item__button">
+          <DeleteForeverIcon />
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default BaggageItem;
