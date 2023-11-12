df7e258a271846f15cc6847d77eb2935fff99d56
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 21:37:08 +0200

    created styles folder and moved there App.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.scss";
+import "./assets/styles/App.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
