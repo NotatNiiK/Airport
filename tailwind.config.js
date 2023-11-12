f59a8703afbbd640231e598f2e9f838feaac070b
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 21:36:26 +0200

    added lexend deca font class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        "lexend-deca": '"Lexend Deca", Arial, Helvetica, sans-serif',
+      },
       boxShadow: {
         "opacity-black": "0px 4px 4px 0px rgba(0, 0, 0, 0.3)",
         "opacity-white": "0px 0px 5px rgba(255,255,255, 0.6)",
