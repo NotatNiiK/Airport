81af78557414ae1bf997fb2419e229af26df984b
Погадайко Антон <pogadajko52@gmail.com>
2023-11-12 14:59:00 +0200

    add Loader to TicketList

--- src/components/TicketsList/TicketsList.tsx
+++ src/components/TicketsList/TicketsList.tsx
@@ -3,16 +3,22 @@ import { ITickets } from "../../models/ticket";
 import cl from "./TicketsList.module.scss";
 import NotFound from "../UI/NotFound/NotFound";
 import TicketItem from "../TicketItem/TicketItem";
+import Loader from "../UI/Loader/Loader";
 
 interface TicketsListProps {
   ticketsList: ITickets;
+  loading: boolean;
 }
 
-const TicketsList: FC<TicketsListProps> = ({ ticketsList }) => {
+const TicketsList: FC<TicketsListProps> = ({ ticketsList, loading }) => {
+  if (loading) {
+    return <Loader />;
+  }
+
   return ticketsList.length > 0 ? (
     <ul className={cl["ticket-list"]}>
-      {ticketsList.map((ticket: any) => (
-        <TicketItem ticket={ticket} />
+      {ticketsList.map((ticket) => (
+        <TicketItem ticket={ticket} key={ticket.id} />
       ))}
     </ul>
   ) : (
