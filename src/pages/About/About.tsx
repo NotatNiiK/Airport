974029c9992d61c1ff3b7f77876fd9f7a2a8cc68
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 10:50:26 +0200

    added ImageLoading to About page and refactoring styles

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,48 +1,47 @@
 import { FC } from "react";
 import cl from "./About.module.scss";
 import Header from "../../components/Header/Header";
+import ImageLoading from "../../components/UI/ImageLoading/ImageLoading";
 import PlainImage from "../../assets/images/pages/About/Plain.png";
 
 const About: FC = () => {
   return (
     <>
       <Header />
-      <main className="main">
-        <section className={cl["about"]}>
-          <div className={cl["about__container"]}>
-            <div className={cl["about__content"]}>
-              <h1 className={cl["about__title"]}>Utravel airport</h1>
-              <p className={cl["about__paragraph"]}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
-                voluptate tenetur eum iste id eos non, quis nesciunt, illum
-                placeat eveniet quidem suscipit officia temporibus perspiciatis
-                nostrum nemo et dolore.
-              </p>
-              <p className={cl["about__paragraph"]}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
-                voluptate tenetur eum iste id eos non, quis nesciunt, illum
-                placeat eveniet quidem suscipit officia temporibus perspiciatis
-                nostrum nemo et dolore.
-              </p>
-              <p className={cl["about__paragraph"]}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
-                voluptate tenetur eum iste id eos non, quis nesciunt, illum
-                placeat eveniet quidem suscipit officia temporibus perspiciatis
-                nostrum nemo et dolore.
-              </p>
-              <p className={cl["about__paragraph"]}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
-                voluptate tenetur eum iste id eos non, quis nesciunt, illum
-                placeat eveniet quidem suscipit officia temporibus perspiciatis
-                nostrum nemo et dolore.
-              </p>
-            </div>
-            <div className={cl["about__image"]}>
-              <img src={PlainImage} alt="Plain" draggable={false} />
-            </div>
+      <section className={cl["about"]}>
+        <div className={cl["about__container"]}>
+          <div className={cl["about__content"]}>
+            <h1 className={cl["about__title"]}>Utravel airport</h1>
+            <p className={cl["about__paragraph"]}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+              voluptate tenetur eum iste id eos non, quis nesciunt, illum
+              placeat eveniet quidem suscipit officia temporibus perspiciatis
+              nostrum nemo et dolore.
+            </p>
+            <p className={cl["about__paragraph"]}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+              voluptate tenetur eum iste id eos non, quis nesciunt, illum
+              placeat eveniet quidem suscipit officia temporibus perspiciatis
+              nostrum nemo et dolore.
+            </p>
+            <p className={cl["about__paragraph"]}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+              voluptate tenetur eum iste id eos non, quis nesciunt, illum
+              placeat eveniet quidem suscipit officia temporibus perspiciatis
+              nostrum nemo et dolore.
+            </p>
+            <p className={cl["about__paragraph"]}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+              voluptate tenetur eum iste id eos non, quis nesciunt, illum
+              placeat eveniet quidem suscipit officia temporibus perspiciatis
+              nostrum nemo et dolore.
+            </p>
           </div>
-        </section>
-      </main>
+          <div className={cl["about__image"]}>
+            <ImageLoading image={PlainImage} />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
