47cb08a663a3e94cc2bb5da1f6ec515e40fd635f
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 12:20:29 +0200

    added separate mocked options data

---
+++ src/data/options.ts
@@ -0,0 +1,15 @@
+import { IOptions } from "../models/option";
+
+export const classOptions: IOptions = [
+  { id: 0, text: "Econom", value: "Econom" },
+  { id: 1, text: "Business", value: "Business" },
+];
+
+export const placeOptions: IOptions = [
+  { id: 0, text: "1", value: "1" },
+  { id: 1, text: "2", value: "2" },
+  { id: 3, text: "22", value: "22" },
+  { id: 4, text: "52", value: "52" },
+  { id: 5, text: "67", value: "67" },
+  { id: 5, text: "99", value: "99" },
+];
