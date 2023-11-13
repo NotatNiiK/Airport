6dfb854d92bf78ee31a8b18f67fb8a40c7044a76
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 12:47:15 +0200

    refactored Auth pages

--- src/pages/Auth/Authorization.tsx
+++ src/pages/Auth/Authorization.tsx
@@ -1,18 +1,18 @@
 import { FC } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { IAuthData } from "../../models/auth";
 import { usePasswordMask } from "../../hooks/usePasswordMask";
 import { useFetching } from "../../hooks/useFetching";
+import { IAuthData } from "../../models/auth";
 import { useAlert } from "../../hooks/useAlert";
 import cl from "./Auth.module.scss";
 import Notify from "../../components/Notify/Notify";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import AuthButton from "../../components/UI/AuthButton/AuthButton";
 import AuthInput from "../../components/UI/AuthInput/AuthInput";
 import AuthStore from "../../store/AuthStore";
 import AuthValidation from "../../validation/AuthValidation";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const Authorization: FC = () => {
   const navigate = useNavigate();
@@ -25,16 +25,18 @@ const Authorization: FC = () => {
     reset,
   } = useForm<IAuthData>();
 
-  const [errorAlert, showAlert] = useAlert();
+  const [errorAlert, showErrorAlert] = useAlert();
   const [isPasswordMask, passwordType, togglePasswordMask] = usePasswordMask();
 
   const [performAuthorization, isLoading] = useFetching(
     async (authData: IAuthData): Promise<void> => {
       const authReponse = await AuthStore.authorization(authData);
+
       if (authReponse.hasError) {
-        showAlert(authReponse.response);
+        showErrorAlert(authReponse.response);
         return;
       }
+
       reset();
       navigate("/");
     }
@@ -45,7 +47,7 @@ const Authorization: FC = () => {
       <section className={cl["auth__body"]}>
         <h1 className={cl["auth__title"]}>Login</h1>
         <form
-          className="auth__form"
+          className={cl["auth__form"]}
           onSubmit={handleSubmit(performAuthorization)}
         >
           <fieldset className={cl["auth__section"]}>
@@ -81,22 +83,22 @@ const Authorization: FC = () => {
             </div>
           </fieldset>
           <fieldset className={cl["auth__section"]}>
-            <AuthButton type="submit" tabIndex={3} loading={isLoading}>
+            <AuthButton tabIndex={3} loading={isLoading}>
               Login
             </AuthButton>
           </fieldset>
           <p className={cl["auth__link"]}>
-            Don't have an account?{" "}
+            <span>Don't have an account?</span>
             <Link to="/signin" tabIndex={4}>
               Sign in
             </Link>
           </p>
-          <Notify
-            show={errorAlert.show}
-            message={errorAlert.message}
-            type="error"
-          />
         </form>
+        <Notify
+          show={errorAlert.show}
+          message={errorAlert.message}
+          type="error"
+        />
       </section>
     </div>
   );
--- src/pages/Auth/Registration.tsx
+++ src/pages/Auth/Registration.tsx
@@ -1,20 +1,20 @@
 import { FC } from "react";
-import { useForm } from "react-hook-form";
-import { InputMask } from "@react-input/mask";
-import { IRegData } from "../../models/auth";
 import { useNavigate, Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { useFetching } from "../../hooks/useFetching";
 import { usePasswordMask } from "../../hooks/usePasswordMask";
 import { useAlert } from "../../hooks/useAlert";
+import { IRegData } from "../../models/auth";
+import { InputMask } from "@react-input/mask";
 import cl from "./Auth.module.scss";
 import Notify from "../../components/Notify/Notify";
 import AuthInput from "../../components/UI/AuthInput/AuthInput";
 import AuthButton from "../../components/UI/AuthButton/AuthButton";
-import AuthValidation from "../../validation/AuthValidation";
-import getRawPhoneNumber from "../../utils/getRawPhoneNumber";
-import AuthStore from "../../store/AuthStore";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import AuthStore from "../../store/AuthStore";
+import AuthValidation from "../../validation/AuthValidation";
+import getRawPhoneNumber from "../../utils/getRawPhoneNumber";
 
 const Registration: FC = () => {
   const navigate = useNavigate();
@@ -27,17 +27,25 @@ const Registration: FC = () => {
     reset,
   } = useForm<IRegData>();
 
-  const [errorAlert, showAlert] = useAlert();
+  const [errorAlert, showErrorAlert] = useAlert();
   const [isPasswordMask, passwordType, togglePasswordMask] = usePasswordMask();
+
+  const maskInputClasses: string = [
+    cl["auth__input"],
+    errors.contactInfo ? cl["validation-error"] : "",
+  ].join(" ");
 
   const [performRegistration, isLoading] = useFetching(
     async (regData: IRegData): Promise<void> => {
       regData.contactInfo = getRawPhoneNumber(regData.contactInfo);
+
       const regResponse = await AuthStore.registration(regData);
+
       if (regResponse.hasError) {
-        showAlert(regResponse.response);
+        showErrorAlert(regResponse.response);
         return;
       }
+
       reset();
       navigate("/");
     }
@@ -48,27 +56,27 @@ const Registration: FC = () => {
       <section className={cl["auth__body"]}>
         <h1 className={cl["auth__title"]}>Sign in</h1>
         <form
-          className="auth__form"
+          className={cl["auth__form"]}
           onSubmit={handleSubmit(performRegistration)}
         >
           <fieldset className={cl["auth__section"]}>
             <AuthInput
-              isError={errors.fullName}
               {...register("fullName", {
                 required: true,
               })}
+              isError={errors.fullName}
               onBlur={() => clearErrors("fullName")}
-              placeholder="Username"
+              placeholder="Full name"
               tabIndex={1}
             />
           </fieldset>
           <fieldset className={cl["auth__section"]}>
             <AuthInput
-              isError={errors.email}
               {...register("email", {
                 required: true,
                 validate: AuthValidation.email,
               })}
+              isError={errors.email}
               onBlur={() => clearErrors("email")}
               placeholder="Email"
               tabIndex={2}
@@ -76,12 +84,12 @@ const Registration: FC = () => {
           </fieldset>
           <fieldset className={cl["auth__section"]}>
             <AuthInput
-              isError={errors.password}
               type={passwordType}
               {...register("password", {
                 required: true,
                 validate: AuthValidation.password,
               })}
+              isError={errors.password}
               onBlur={() => clearErrors("password")}
               placeholder="Password"
               tabIndex={3}
@@ -96,11 +104,11 @@ const Registration: FC = () => {
           </fieldset>
           <fieldset className={cl["auth__section"]}>
             <AuthInput
-              isError={errors.passportNumber}
               {...register("passportNumber", {
                 required: true,
                 validate: AuthValidation.passportNumber,
               })}
+              isError={errors.passportNumber}
               onBlur={() => clearErrors("passportNumber")}
               placeholder="Passport number"
               tabIndex={4}
@@ -108,38 +116,35 @@ const Registration: FC = () => {
           </fieldset>
           <fieldset className={cl["auth__section"]}>
             <InputMask
-              type="text"
-              className={`${cl["auth__input"]} ${
-                errors.contactInfo ? cl["validation-error"] : ""
-              }`}
-              mask="+38 (___) ___-__-__"
-              replacement={{ _: /\d/ }}
               {...register("contactInfo", {
                 required: true,
                 validate: AuthValidation.myContacts,
               })}
+              className={maskInputClasses}
               onBlur={() => clearErrors("contactInfo")}
+              mask="+38 (___) ___-__-__"
+              replacement={{ _: /\d/ }}
               placeholder="Tel"
               tabIndex={5}
             />
           </fieldset>
           <fieldset className={cl["auth__section"]}>
-            <AuthButton type="submit" tabIndex={6} loading={isLoading}>
+            <AuthButton tabIndex={6} loading={isLoading}>
               Sign in
             </AuthButton>
           </fieldset>
           <p className={cl["auth__link"]}>
-            Do you have an account?{" "}
+            <span>Do you have an account?</span>
             <Link to="/login" tabIndex={7}>
               Login
             </Link>
           </p>
-          <Notify
-            show={errorAlert.show}
-            message={errorAlert.message}
-            type="error"
-          />
         </form>
+        <Notify
+          show={errorAlert.show}
+          message={errorAlert.message}
+          type="error"
+        />
       </section>
     </div>
   );
