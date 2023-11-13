1d42c4d6694711ab6a750c3743bbcc47e3162765
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 11:20:07 +0200

    refactored FormInput component

--- src/components/UI/FormInput/FormInput.tsx
+++ src/components/UI/FormInput/FormInput.tsx
@@ -2,15 +2,15 @@ import { FC, InputHTMLAttributes, forwardRef, Ref } from "react";
 import { FieldError } from "react-hook-form";
 import cl from "./FormInput.module.scss";
 
-interface AuthInputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
   isError?: FieldError;
 }
 
-const FormInput: FC<AuthInputProps> = forwardRef(
+const FormInput: FC<FormInputProps> = forwardRef(
   ({ isError, ...props }, ref: Ref<HTMLInputElement>) => {
     const inputClasses: string = [
       cl["form-input"],
-      `${isError ? cl["validation-error"] : ""}`,
+      isError ? cl["validation-error"] : "",
     ].join(" ");
 
     return <input className={inputClasses} ref={ref} {...props} />;
