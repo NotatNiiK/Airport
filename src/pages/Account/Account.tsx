60afc39aa57657a91fc4514fba503da90c855a30
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 14:50:04 +0200

    added error handling to Account page and updated styles

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,15 +1,31 @@
-import { FC, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
 import { useFetching } from "../../hooks/useFetching";
+import { IAlert } from "../../models/alert";
 import cl from "./Account.module.scss";
-import TicketStore from "../../store/TicketStore";
-import AuthStore from "../../store/AuthStore";
 import Header from "../../components/Header/Header";
 import TicketsList from "../../components/TicketsList/TicketsList";
+import TicketStore from "../../store/TicketStore";
+import AuthStore from "../../store/AuthStore";
+import { Alert } from "@mui/material";
 
 const Account: FC = () => {
+  const [errorAlert, setErrorAlert] = useState<IAlert>({
+    show: false,
+    message: "",
+  });
+
   const [getTickets, isLoading] = useFetching(async (): Promise<void> => {
-    const userId = AuthStore.tokenInfo.id || 0;
-    await TicketStore.getTickets(userId);
+    if (AuthStore.tokenInfo?.id) {
+      const getTicketsResponse = await TicketStore.getTickets(
+        AuthStore.tokenInfo.id
+      );
+      if (getTicketsResponse?.hasError) {
+        setErrorAlert({
+          show: true,
+          message: getTicketsResponse.response,
+        });
+      }
+    }
   });
 
   useEffect(() => {
@@ -24,10 +40,23 @@ const Account: FC = () => {
             <h1 className={cl["account__title"]}>
               Welcome, {AuthStore.tokenInfo.username}
             </h1>
-            <TicketsList
-              ticketsList={TicketStore.ticketsList}
-              loading={isLoading}
-            />
+            {errorAlert.show ? (
+              <Alert
+                severity="error"
+                sx={{
+                  fontSize: "20px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                {errorAlert.message}
+              </Alert>
+            ) : (
+              <TicketsList
+                ticketsList={TicketStore.ticketsList}
+                isLoading={isLoading}
+              />
+            )}
           </div>
         </div>
       </main>
