f08ddade5923a38f7167cbdb86e941f72b6a9fae
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 15:00:36 +0200

    refactored Baggage page

--- src/pages/Baggage/Baggage.tsx
+++ src/pages/Baggage/Baggage.tsx
@@ -1,23 +1,38 @@
-import { FC, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { observer } from "mobx-react-lite";
+import { useModal } from "../../hooks/useModal";
+import { useFetching } from "../../hooks/useFetching";
+import { IAlert } from "../../models/alert";
+import { Alert } from "@mui/material";
 import cl from "./Baggage.module.scss";
 import Header from "../../components/Header/Header";
-import Modal from "../../components/UI/Modal/Modal";
-import { useParams } from "react-router-dom";
-import { useModal } from "../../hooks/useModal";
 import GeneralBaggageForm from "../../components/Forms/GeneralForm/GeneralBaggageForm";
 import BaggageList from "../../components/BaggageList/BaggageList";
-import { useFetching } from "../../hooks/useFetching";
+import Modal from "../../components/UI/Modal/Modal";
 import BaggageStore from "../../store/BaggageStore";
 
 const Baggage: FC = observer(() => {
   const { tiketId } = useParams();
   const [isCreateModalOpen, toggleCreateModal] = useModal();
 
+  const [errorAlert, setErrorAlert] = useState<IAlert>({
+    show: false,
+    message: "",
+  });
+
   const [getBaggage, isLoading] = useFetching(async (): Promise<void> => {
     if (tiketId) {
       const numberTicketId = +tiketId;
-      await BaggageStore.getBaggageById(numberTicketId);
+      const getBaggageResponse = await BaggageStore.getBaggageById(
+        numberTicketId
+      );
+      if (getBaggageResponse?.hasError) {
+        setErrorAlert({
+          show: true,
+          message: getBaggageResponse.response,
+        });
+      }
     }
   });
 
@@ -40,12 +55,27 @@ const Baggage: FC = observer(() => {
               Add baggage +
             </button>
           </div>
-          <BaggageList baggages={BaggageStore.baggages} isLoading={isLoading} />
+          {errorAlert.show ? (
+            <Alert
+              severity="error"
+              sx={{
+                fontSize: "20px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {errorAlert.message}
+            </Alert>
+          ) : (
+            <BaggageList
+              baggages={BaggageStore.baggages}
+              isLoading={isLoading}
+            />
+          )}
         </section>
         <Modal open={isCreateModalOpen} toggleModal={toggleCreateModal}>
           <GeneralBaggageForm
             title="Create Baggage"
-            isEdit={false}
             closeModal={toggleCreateModal}
             isClearForm={true}
           />
