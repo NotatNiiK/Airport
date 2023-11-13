b713ccd1ad404d08893d5a48a2ef83ed05c69414
Погадайко Антон <pogadajko52@gmail.com>
2023-11-13 14:31:15 +0200

    update BaggageList styles and added new design for BaggageItem

--- src/components/BaggageItem/BaggageItem.tsx
+++ src/components/BaggageItem/BaggageItem.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
-import { IBaggage } from "../../models/baggage";
 import { useParams } from "react-router-dom";
-import cl from "./BaggageItem.module.scss";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import Modal from "../UI/Modal/Modal";
 import { useModal } from "../../hooks/useModal";
+import { useFetching } from "../../hooks/useFetching";
+import { useAlert } from "../../hooks/useAlert";
+import { IBaggage } from "../../models/baggage";
+import cl from "./BaggageItem.module.scss";
 import GeneralBaggageForm from "../Forms/GeneralForm/GeneralBaggageForm";
 import ConfirmForm from "../Forms/ConfirmForm/ConfirmForm";
-import { useFetching } from "../../hooks/useFetching";
+import Notify from "../UI/Notify/Notify";
+import Modal from "../UI/Modal/Modal";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import BaggageStore from "../../store/BaggageStore";
 
 interface BaggageItemProps {
@@ -18,39 +20,56 @@ interface BaggageItemProps {
 const BaggageItem: FC<BaggageItemProps> = ({ baggage }) => {
   const { tiketId } = useParams();
 
+  const [errorAlert, showErrorAlert] = useAlert();
+
   const [isEditModalOpen, toggleEditModal] = useModal();
   const [isDeleteModalOpen, toggleDeleteModal] = useModal();
 
   const [deleteBaggage, isLoading] = useFetching(async (): Promise<void> => {
     const baggageId = baggage.id || 0;
-    await BaggageStore.deleteBaggage(baggageId);
+    const deleteBaggageResponse = await BaggageStore.deleteBaggage(baggageId);
+
+    if (deleteBaggageResponse.hasError) {
+      showErrorAlert(deleteBaggageResponse.response);
+      return;
+    }
+
     if (tiketId) {
-      const ticketID = +tiketId;
-      await BaggageStore.getBaggageById(ticketID);
+      const numberTicketId = +tiketId;
+      await BaggageStore.getBaggageById(numberTicketId);
     }
   });
 
   return (
     <li className={cl["baggage-item"]}>
+      <h2 className={cl["baggage-item__title"]}>Baggage information</h2>
       <h2 className={cl["baggage-item__cost"]}>Cost: {baggage.cost}$</h2>
-      <ul className={cl["baggage-item__list"]}>
-        <li className={cl["baggage-item__list-item"]}>
-          Weight: {baggage.weight}kg
-        </li>
-        <li className={cl["baggage-item__list-item"]}>
-          Height: {baggage.height}cm
-        </li>
-        <li className={cl["baggage-item__list-item"]}>
-          Width: {baggage.width}cm
-        </li>
-      </ul>
-      <div className="baggage-item__buttons">
-        <button className="baggage-item__button" onClick={toggleEditModal}>
-          <EditNoteIcon />
-        </button>
-        <button className="baggage-item__button" onClick={toggleDeleteModal}>
-          <DeleteForeverIcon />
-        </button>
+      <div className={cl["baggage-item__content"]}>
+        <ul className={cl["baggage-item__list"]}>
+          <li className={cl["baggage-item__list-item"]}>
+            Weight: {baggage.weight}kg
+          </li>
+          <li className={cl["baggage-item__list-item"]}>
+            Height: {baggage.height}cm
+          </li>
+          <li className={cl["baggage-item__list-item"]}>
+            Width: {baggage.width}cm
+          </li>
+        </ul>
+        <div className={cl["baggage-item__buttons"]}>
+          <button
+            className={cl["baggage-item__button"]}
+            onClick={toggleEditModal}
+          >
+            <EditNoteIcon sx={{ fontSize: "35px" }} />
+          </button>
+          <button
+            className={cl["baggage-item__button"]}
+            onClick={toggleDeleteModal}
+          >
+            <DeleteForeverIcon sx={{ fontSize: "35px" }} />
+          </button>
+        </div>
       </div>
       <Modal open={isEditModalOpen} toggleModal={toggleEditModal}>
         <GeneralBaggageForm
@@ -69,6 +88,11 @@ const BaggageItem: FC<BaggageItemProps> = ({ baggage }) => {
           loading={isLoading}
         ></ConfirmForm>
       </Modal>
+      <Notify
+        show={errorAlert.show}
+        message={errorAlert.message}
+        type="error"
+      ></Notify>
     </li>
   );
 };
